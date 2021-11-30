d61cef8c82b20033256c93e61182087f222719d8
魏有成 <961526766@qq.com>
2021-11-30 14:28:35 +0800

    docs: docs/promisify 同步 remote-git-tags-analysis :construction:

--- remote-git-tags-analysis/remote-git-tags/index.js
+++ remote-git-tags-analysis/remote-git-tags/index.js
@@ -1,25 +1,26 @@
+import {
+	promisify
+} from 'node:util';
 import childProcess from 'node:child_process';
 
+const execFile = promisify(childProcess.execFile);
+
 export default async function remoteGitTags(repoUrl) {
-	return new Promise((resolve, reject) => {
-		const process = childProcess.execFile('git', ['ls-remote', '--tags', repoUrl], (error, stdout) => {
-			console.log(process.pid, '=========pid==========');
-			if (error) {
-				reject(error);
-			}
+	const {
+		stdout
+	} = await execFile('git', ['ls-remote', '--tags', repoUrl]);
+	const tags = new Map();
 
-			const tags = new Map();
-			for (const line of stdout.trim().split('\n')) {
-				const [hash, tagReference] = line.split('\t');
-				// Strip off the indicator of dereferenced tags so we can override the
-				// previous entry which points at the tag hash and not the commit hash
-				// `refs/tags/v9.6.0^{}` → `v9.6.0`
-				const tagName = tagReference.replace(/^refs\/tags\//, '').replace(/\^{}$/, '');
+	for (const line of stdout.trim().split('\n')) {
+		const [hash, tagReference] = line.split('\t');
 
-				tags.set(tagName, hash);
-			}
+		// Strip off the indicator of dereferenced tags so we can override the
+		// previous entry which points at the tag hash and not the commit hash
+		// `refs/tags/v9.6.0^{}` → `v9.6.0`
+		const tagName = tagReference.replace(/^refs\/tags\//, '').replace(/\^{}$/, '');
 
-			resolve(tags);
-		});
-	});
+		tags.set(tagName, hash);
+	}
+
+	return tags;
 }
